c5ad8aad45fe2fad54018c9ab7a67d37b8cc331c
kenta.aramaki <mgk125ap@gmail.com>
2022-09-21 15:22:19 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,10 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "messages")
 public class Message {
     @Id
@@ -51,7 +58,7 @@ public class Message {
     }
 
     public void setContent(String content) {
-        this.content =content;
+        this.content = content;
     }
 
     public Timestamp getCreated_at() {
@@ -69,5 +76,4 @@ public class Message {
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
-
 }
